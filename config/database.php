456f425c611d7d77ef32950318ab98840b479b45
Korpong <33505027+korpongg@users.noreply.github.com>
2026-08-06 14:37:09 +0700

    Fix HTTPS asset URL for Render deployment

--- config/database.php
+++ config/database.php
@@ -59,7 +59,7 @@ return [
             'strict' => true,
             'engine' => null,
             'options' => extension_loaded('pdo_mysql') ? array_filter([
-                PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
+                 PDO::ATTR_PERSISTENT => false,
             ]) : [],
         ],
 
@@ -75,7 +75,7 @@ return [
             'prefix' => '',
             'prefix_indexes' => true,
             'search_path' => 'public',
-            'sslmode' => 'prefer',
+             'sslmode' => 'require',
         ],
 
         'sqlsrv' => [
